e94f79e0b2a4043419e0c41890d1e96d4c2d930a
Sebastian Manco <sebasmg1393@gmail.com>
2020-12-07 22:54:49 -0500

    mi primer carrito version final

---
+++ js/index.js
@@ -0,0 +1,165 @@
+//acceder y leer la apijson
+
+//seccion de elementos captados
+const cards = document.getElementById('cards');
+const items = document.getElementById('items');
+const footer = document.getElementById('footer');
+const templateCard = document.getElementById('template-card').content
+const templateFooter = document.getElementById('template-footer').content
+const templateCarrito = document.getElementById('template-carrito').content
+const fragment = document.createDocumentFragment();
+let carrito = {
+
+}
+// Eventos
+// El evento DOMContentLoaded es disparado cuando el documento HTML ha sido completamente cargado y parseado
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetchData();
+})
+
+cards.addEventListener('click', e => {
+    addCarrito(e);
+})
+
+items.addEventListener('click', e => {
+    btnAccion(e)
+})
+
+// Traer productos
+
+const fetchData = async () => {
+    try {
+        const res = await fetch("./js/api.json");
+        const data = await res.json();
+        //console.log(data);
+        pintarCard(data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// Pintar productos
+const pintarCard = data => {
+    data.forEach(producto => {
+        templateCard.querySelector('h5').textContent = producto.title;
+        templateCard.querySelector('p').textContent = producto.precio;
+        templateCard.querySelector('img').setAttribute("src", producto.thumbnailUrl);
+        templateCard.querySelector('.btn-dark').dataset.id = producto.id;
+        const clone = templateCard.cloneNode(true);
+        fragment.appendChild(clone);
+
+
+    });
+    cards.appendChild(fragment);
+}
+
+
+const addCarrito = e => {
+    //console.log(e.target);
+    //console.log(e.target.classList.contains('btn-dark'))
+    if (e.target.classList.contains('btn-dark')) {
+
+        //console.log(e.target.parentElement);
+        setCarrito(e.target.parentElement);
+    }
+    e.stopPropagation()
+}
+
+const setCarrito = objeto => {
+    //console.log(objeto);
+    const producto = {
+        id: objeto.querySelector('.btn-dark').dataset.id,
+        title: objeto.querySelector('h5').textContent,
+        precio: objeto.querySelector('p').textContent,
+        cantidad: 1
+    }
+
+    if (carrito.hasOwnProperty(producto.id)) {
+        producto.cantidad = carrito[producto.id].cantidad + 1;
+    }
+
+    carrito[producto.id] = { ...producto };
+    pintarCarrito();
+    //console.log(carrito);
+}
+
+//pintar carrito
+
+const pintarCarrito = () => {
+    //console.log(carrito);
+    items.innerHTML = '';
+    Object.values(carrito).forEach(producto => {
+        templateCarrito.querySelector('th').textContent = producto.id
+        templateCarrito.querySelectorAll('td')[0].textContent = producto.title
+        templateCarrito.querySelectorAll('td')[1].textContent = producto.cantidad
+        templateCarrito.querySelector('.btn-info').dataset.id = producto.id
+        templateCarrito.querySelector('.btn-danger').dataset.id = producto.id
+        templateCarrito.querySelector('span').textContent = producto.cantidad * producto.precio
+
+        const clone = templateCarrito.cloneNode(true);
+        fragment.appendChild(clone);
+    })
+
+    items.appendChild(fragment);
+
+    pintarFooter()
+}
+
+const pintarFooter = () => {
+    footer.innerHTML = ''
+    // Limpia el carrito 
+    if (Object.keys(carrito).length === 0) {
+        footer.innerHTML = `
+        <th scope="row" colspan="5">Carrito vacío - comience a comprar!</th>
+        `
+        return
+    }
+
+    const nCantidad = Object.values(carrito).reduce((acc, { cantidad }) => acc + cantidad, 0)
+    const nPrecio = Object.values(carrito).reduce((acc, { cantidad, precio }) => acc + cantidad * precio, 0)
+    //console.log(nPrecio);
+    //console.log(nCantidad);
+
+    templateFooter.querySelectorAll('td')[0].textContent = nCantidad
+    templateFooter.querySelector('span').textContent = nPrecio
+
+    const clone = templateFooter.cloneNode(true)
+    fragment.appendChild(clone)
+    footer.appendChild(fragment)
+
+    const btnVaciar = document.getElementById('vaciar-carrito')
+    btnVaciar.addEventListener('click', () => {
+        carrito = {}
+        pintarCarrito()
+    })
+
+}
+
+const btnAccion = e => {
+    console.log(e.target)
+    //Accion de aumentar
+    if (e.target.classList.contains('btn-info')) {
+        //carrito[e.target.dataset.id]
+        //console.log(carrito[e.target.dataset.id])
+        const producto = carrito[e.target.dataset.id]
+        producto.cantidad++
+        carrito[e.target.dataset.id] = { ...producto }
+        pintarCarrito()
+    }
+
+    if (e.target.classList.contains('btn-danger')) {
+        //carrito[e.target.dataset.id]
+        //console.log(carrito[e.target.dataset.id])
+        const producto = carrito[e.target.dataset.id]
+        producto.cantidad--
+        if (producto.cantidad === 0) {
+            delete carrito[e.target.dataset.id]
+        }
+
+        pintarCarrito()
+
+    }
+
+    e.stopPropagation()
+}
